9c129f4a12c9f5e11c9b5709bd2ca230eecd4f89
mouadh7belgoumri <m_belgoumri@estin.dz>
2025-06-13 18:35:17 +0100

    database setup

--- routes/products.js
+++ routes/products.js
@@ -1,29 +1,29 @@
 import express from 'express'
 import {getProducts, getProduct, addProduct} from '../dbFunctions.js';
-const router = express.Router();
+const productsRouter = express.Router();
 
 const connectionData = {
     host: 'localhost',
     user: 'root',
     password: process.env.DATABASE_PASSWORD,
-    database: 'products'
+    database: 'Alloras'
 }
 
-router.get("/", async (req,res)=>{
+productsRouter.get("/", async (req,res)=>{
     res.send(await getProducts(connectionData))
 })
 
-router.get('/:id', async (req, res)=>{
+productsRouter.get('/:id', async (req, res)=>{
     const rows = await getProducts(connectionData);
     
     
     res.send(rows[req.params.id - 1])
 })
 
-router.post('/', async (req, res)=>{
+productsRouter.post('/', async (req, res)=>{
     res.send(await addProduct(connectionData));
 })
 
 
 
-export default router;
+export default productsRouter;
